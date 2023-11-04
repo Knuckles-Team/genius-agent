b5cf2d040f6dcf777baacc398da01067373c9740
knucklessg1 <knucklessg1@gmail.com>
2023-11-04 17:45:06 -0500

    Checking in changes.

--- genius_agent/structs/agent_structs.py
+++ genius_agent/structs/agent_structs.py
@@ -5,25 +5,26 @@ from autogen import (AssistantAgent as AutoAssistantAgent,
                      UserProxyAgent as AutoUserProxyAgent,
                      GroupChat as AutoGroupChat,
                      GroupChatManager as AutoGroupChatManager)
-from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
-from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
-from autogen.agentchat.contrib.teachable_agent import TeachableAgent
-from llm_config_structs import LLMConfig
-from ..agent_functions import *
+from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent as AutoRetrieveAssistantAgent
+from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent as AutoRetrieveUserProxyAgent
+from autogen.agentchat.contrib.teachable_agent import TeachableAgent as AutoTeachableAgent
+from structs.llm_config_structs import LLMConfig
+from agent_functions import *
 import os
 import importlib.util
 
-# Import all custom agent functions
-current_directory = os.path.dirname(os.path.realpath(__file__))
-agent_functions_path = os.path.join(current_directory, '..', 'agent_functions')
-python_files = [f for f in os.listdir(agent_functions_path) if f.endswith('.py')]
-for file_name in python_files:
-    module_name = os.path.splitext(file_name)[0]
-    module_path = os.path.join(agent_functions_path, file_name)
 
-    spec = importlib.util.spec_from_file_location(module_name, module_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
+# # Import all custom agent functions
+# current_directory = os.path.dirname(os.path.realpath(__file__))
+# agent_functions_path = os.path.join(current_directory, '..', 'agent_functions')
+# python_files = [f for f in os.listdir(agent_functions_path) if f.endswith('.py')]
+# for file_name in python_files:
+#     module_name = os.path.splitext(file_name)[0]
+#     module_path = os.path.join(agent_functions_path, file_name)
+#
+#     spec = importlib.util.spec_from_file_location(module_name, module_path)
+#     module = importlib.util.module_from_spec(spec)
+#     spec.loader.exec_module(module)
 
 
 class CodeExecutionConfig(BaseModel):
@@ -48,7 +49,7 @@ class TeachConfig(BaseModel):
 
 class Agent(BaseModel):
     name: str
-    llm_config: str
+    llm_config: dict
     system_message: Optional[str]
     is_termination_msg: Optional[str]
     human_input_mode: Optional[str] = "NEVER"
@@ -59,63 +60,70 @@ class Agent(BaseModel):
 
 
 class UserProxyAgent(Agent):
-    AutoUserProxyAgent(
-        name=Agent.name,
-        is_termination_msg=Agent.is_termination_msg or None,
-        human_input_mode=Agent.human_input_mode or None,
-        system_message=Agent.system_message or None,
-        code_execution_config=Agent.code_execution_config or None,
-    ).register_function(
-        function_map={
-            "python": python_func.exec_python,
-            "bash": bash_func.exec_bash,
-            "media_download": media_downloader_func.exec_media_downloader,
-            "write_to_file": file_func.exec_write_to_file,
-            "read_from_file": file_func.exec_read_from_file,
-            "create_directory": file_func.exec_create_directory,
-        }
-    )
+    def __init__(self):
+        super().__init__()
+        self.agent = (AutoUserProxyAgent(
+            name=self.name,
+            is_termination_msg=self.is_termination_msg,
+            human_input_mode=self.human_input_mode,
+            system_message=self.system_message,
+            code_execution_config=self.code_execution_config
+        ))
+
+    def map_functions(self, functions_mapping):
+        self.user_proxy_agent.register_function(
+            function_map=functions_mapping
+        )
 
 
 class AssistantAgent(Agent):
-    AutoAssistantAgent(
-        name=Agent.name,
-        is_termination_msg=Agent.is_termination_msg or None,
-        system_message=Agent.system_message or None,
-        llm_config=Agent.llm_config,
-    )
+    def __init__(self):
+        super().__init__()
+        self.agent = (AutoAssistantAgent(
+            name=self.name,
+            is_termination_msg=self.is_termination_msg,
+            system_message=self.system_message,
+            code_execution_config=self.code_execution_config,
+            llm_config=Agent.llm_config,
+        ))
 
 
 class RetrieveUserProxyAgent(Agent):
-    RetrieveUserProxyAgent(
-        name=Agent.name,
-        is_termination_msg=Agent.is_termination_msg or None,
-        system_message=Agent.system_message or None,
-        llm_config=Agent.llm_config,
-        human_input_mode=Agent.human_input_mode or None,
-        max_consecutive_auto_reply=Agent.max_consecutive_auto_reply or None,
-        retrieve_config=Agent.retrieve_config or None,
-        code_execution_config=Agent.code_execution_config or None,
-    )
+    def __init__(self):
+        super().__init__()
+        self.agent = (AutoRetrieveUserProxyAgent(
+            name=self.name,
+            is_termination_msg=self.is_termination_msg,
+            system_message=self.system_message,
+            uman_input_mode=self.human_input_mode or None,
+            code_execution_config=self.code_execution_config,
+            retrieve_config=self.retrieve_config or None,
+            llm_config=self.llm_config,
+        ))
 
 
 class RetrieveAssistantAgent(Agent):
-    RetrieveAssistantAgent(
-        name=Agent.name,
-        is_termination_msg=Agent.is_termination_msg or None,
-        system_message=Agent.system_message or None,
-        llm_config=Agent.llm_config,
-    )
+    def __init__(self):
+        super().__init__()
+        self.agent = (AutoRetrieveAssistantAgent(
+            name=self.name,
+            is_termination_msg=self.is_termination_msg or None,
+            system_message=self.system_message or None,
+            llm_config=self.llm_config,
+        ))
 
 
 class TeachableAgent(Agent):
-    TeachableAgent(
-        name=Agent.name,
-        is_termination_msg=Agent.is_termination_msg or None,
-        system_message=Agent.system_message or None,
-        llm_config=Agent.llm_config,
-        teach_config=Agent.teach_config
-    )
+    def __init__(self):
+        super().__init__()
+        self.agent = (AutoTeachableAgent(
+            name=self.name,
+            is_termination_msg=self.is_termination_msg or None,
+            system_message=self.system_message or None,
+            llm_config=self.llm_config,
+            teach_config=self.teach_config
+        ))
+
 
 
 class Agents(BaseModel):
@@ -135,9 +143,9 @@ class Agents(BaseModel):
         ))
 
 
-class GroupChat(Agents):
+class GroupChat(BaseModel):
     AutoGroupChat(agents=Agents.get_all_agents(), messages=[], max_round=12)
 
 
-class Manager(GroupChat):
+class Manager(BaseModel):
     AutoGroupChatManager(groupchat=GroupChat, llm_config=LLMConfig)
