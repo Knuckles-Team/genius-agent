5662c28b7cb82ed8c88cd2d85b1c8c1783d92f04
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-09 23:56:13 -0500

    Bump version: 2.13.39 → 2.13.40

--- genius_agent/__main__.py
+++ genius_agent/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # coding: utf-8
-from genius_agent.agent import agent_server
+from genius_agent.agent_server import agent_server
 
 if __name__ == "__main__":
     agent_server()
--- genius_agent/agent_server.py
+++ genius_agent/agent_server.py
@@ -11,7 +11,7 @@ from agent_utilities import (
     load_identity,
 )
 
-__version__ = "2.13.38"
+__version__ = "2.13.40"
 
 logging.basicConfig(
     level=logging.INFO,
