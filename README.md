a21ddbd6917974fc631ef9d5fdf44b99eabf400c
knucklessg1 <knucklessg1@gmail.com>
2026-05-22 02:52:10 -0500

    Bump version: 2.22.1 → 2.23.0

--- README.md
+++ README.md
@@ -20,7 +20,7 @@
 ![PyPI - Wheel](https://img.shields.io/pypi/wheel/genius-agent)
 ![PyPI - Implementation](https://img.shields.io/pypi/implementation/genius-agent)
 
-*Version: 2.22.1*
+*Version: 2.23.0*
 
 ---
 
--- genius_agent/agent_server.py
+++ genius_agent/agent_server.py
@@ -20,7 +20,7 @@ warnings.filterwarnings("ignore", message=".*urllib3.*or charset_normalizer.*")
 warnings.filterwarnings("ignore", category=DeprecationWarning, module="fastmcp")
 
 
-__version__ = "2.22.1"
+__version__ = "2.23.0"
 
 logging.basicConfig(
     level=logging.INFO,
